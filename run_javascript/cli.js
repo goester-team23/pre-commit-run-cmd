f00ad29302e163e445c17636f040c4fb64bbebfb
David Danier <danier@team23.de>
2022-08-26 19:23:15 +0200

    Add javascript support

---
+++ run_javascript/cli.js
@@ -0,0 +1,20 @@
+const { execFile } = require('node:child_process');
+
+if (process.argv.length < 3) {
+  console.log('Usage: node cli.js <cmd> ...');
+  process.exit(1);
+}
+
+let cmd = process.argv[2];
+let args = process.argv.slice(3);
+
+execFile(cmd, args, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  if (stdout)
+    console.log(stdout);
+  if (stderr)
+    console.error(stderr);
+});
